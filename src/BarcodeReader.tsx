8181d5b35bd13c22beb19fd5fa652b2893c133e0
Santiago Orrego Díaz <santiago.orrego@softwareone.com>
2025-01-08 07:12:36 -0500

    update package

--- src/BarcodeReader.tsx
+++ src/BarcodeReader.tsx
@@ -1,59 +1,36 @@
-import React, { useEffect, useRef, useState } from "react";
-import { View, Text, PermissionsAndroid, Platform } from "react-native";
+import React from "react";
+import { StyleSheet } from "react-native";
 import { Camera, useCameraDevices } from "react-native-vision-camera";
 
-interface BarcodeReaderProps {
-  onBarcodeRead: (event: any) => void;
-}
-
 const BarcodeReader: React.FC = () => {
-  const HandleOnBarcodeRead = () => {
-    console.log("BarcodeReader");
-  };
+  const devices = useCameraDevices("wide-angle-camera");
+  const device = devices.back;
 
-  return <View style={{ flex: 1 }}>{HandleOnBarcodeRead()}</View>;
-  // const devices = useCameraDevices();
-  // const device = devices.back;
-  // const cameraRef = useRef(null);
-  // const [hasPermission, setHasPermission] = useState(false);
+  const { hasPermission } = useCameraPermission();
 
-  // useEffect(() => {
-  //   const requestCameraPermission = async () => {
-  //     if (Platform.OS === 'android') {
-  //       const status = await PermissionsAndroid.request(
-  //         PermissionsAndroid.PERMISSIONS.CAMERA,
-  //       );
-  //       setHasPermission(status === PermissionsAndroid.RESULTS.GRANTED);
-  //     } else {
-  //       const status = await Camera.requestCameraPermission();
-  //       setHasPermission(status === 'authorized');
-  //     }
-  //   };
+  const codeScanner = useCodeScanner({
+    codeTypes: ["qr", "ean-13"],
+    onCodeScanned: (codes) => {
+      console.log(`Scanned ${codes.length} codes!`);
+    },
+  });
 
-  //   requestCameraPermission();
-  // }, []);
-
-  // const handleBarcodeDetected = ({ nativeEvent }: { nativeEvent: any }) => {
-  //   if (onBarcodeRead) {
-  //     onBarcodeRead(nativeEvent);
-  //   }
-  // };
-
-  // if (device == null || !hasPermission) {
-  //   return <Text>Sin acceso a la cámara</Text>;
-  // }
-
-  // return (
-  //   <View style={{ flex: 1 }}>
-  //     <Camera
-  //       ref={cameraRef}
-  //       style={{ flex: 1 }}
-  //       device={device}
-  //       isActive={true}
-  //       onBarcodeDetected={handleBarcodeDetected}
-  //     />
-  //   </View>
-  // );
+  if (!hasPermission) return <></>;
+  if (device == null) return <></>;
+  return (
+    <Camera
+      style={styles.absoluteFill}
+      device={device}
+      isActive={true}
+      codeScanner={codeScanner}
+    />
+  );
 };
+
+const styles = StyleSheet.create({
+  absoluteFill: {
+    ...StyleSheet.absoluteFill,
+  },
+});
 
 export default BarcodeReader;
